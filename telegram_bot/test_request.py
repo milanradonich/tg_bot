cb4806e6aefd12f0034af3ebf09dbedd637569f5
milanradonich <mradonich90@gmail.com>
2023-08-28 22:26:41 +0300

    work_upd

--- telegram_bot/test_request.py
+++ telegram_bot/test_request.py
@@ -3,22 +3,22 @@ import json
 import re
 import config
 
-my_url = "https://hotels4.p.rapidapi.com/locations/v3/search"
+my_url = "https://hotels4.p.rapidapi.com/locations/search"
 
 headers = {
     "X-RapidAPI-Key": config.RAPID_API_KEY,
     "X-RapidAPI-Host": "hotels4.p.rapidapi.com"
 }
 
-
 pattern = '<[^>]*'
-querystring = {"q": 'rome', "locale": "en_US", "currency": "USD"}
+querystring = {"query": "milan", "locale": "en_US", "currency": "USD"}
 response = requests.get(my_url, headers=headers, params=querystring)
 data = json.loads(response.text)  # десериализация JSON
-# with open('descr_city.json', 'a') as city_file:
-#     json.dump(data, city_file, indent=4)  # сериализация JSON
+with open('descr_city.json', 'w') as city_file:
+    json.dump(data, city_file, indent=4)  # сериализация JSON
 
 possible_city = {}
 for i_city in data['suggestions'][0]['entities']:
     possible_city[i_city['destinationId']] = re.sub(pattern, '', i_city['caption'])
 print(possible_city)
+
